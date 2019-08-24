00cddf64eafdd6702fbf7eb1155515245861bbf5
Juank <jcgomez042@gmail.com>
2019-08-24 18:40:52 -0500

    protect routes

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,16 @@ const User = mongoose.model('User');
 const lineReader = require('line-reader');
 const fs = require('fs');
 
-/* GET home page. */
+/**
+ * Home route, to test if app is running
+ */
 router.get('/', function(req, res, next) {
   return res.status(200).send({ message: 'Express' });
 });
 
+/**
+ * Route to get information about current user logged
+ */
 router.get('/me', authMiddleware.ensureAuthenticated, function(req, res, next) {
   User.findOne({_id: mongoose.Types.ObjectId(req.user)}, function (err, user) {
 		if (err){
@@ -31,6 +36,10 @@ router.get('/me', authMiddleware.ensureAuthenticated, function(req, res, next) {
 	});  
 });
 
+/**
+ * Function to sotre a list of array in a sorted.txt file
+ * @param {array} arr List of arrays to store
+ */
 let writeSortedFile = (arr) => {
   let stream = fs.createWriteStream("public/assets/sorted.txt");
   stream.once('open', function(fd) {
@@ -41,6 +50,11 @@ let writeSortedFile = (arr) => {
   });
 } 
 
+/**
+ * Function to read original file contains list of arrays
+ * @param {function} nsort function to sort arrays
+ * @param {*} mix flag to alternate sorting
+ */
 let readOriginalFile = (nsort, mix = false) => {
   if(typeof nsort !== "function"){
     console.log("Please, You must to give a function to sort the arrays");
@@ -50,7 +64,6 @@ let readOriginalFile = (nsort, mix = false) => {
   let i = 1;
   lineReader.eachLine('public/assets/original.txt', function(line, last) {
     let arr = JSON.parse(line.replace(";", ""));
-
     let sortedArr = arr.sort(nsort);
     if(mix && i % 2 === 0){
       sortedArr.reverse();
@@ -63,21 +76,30 @@ let readOriginalFile = (nsort, mix = false) => {
   });
 }
 
-router.get('/asc', logMiddleware.storeLog, function(req, res, next){
+/**
+ * Route to sort arrays ascendig
+ */
+router.get('/asc', [authMiddleware.ensureAuthenticated, logMiddleware.storeLog], function(req, res, next){
   readOriginalFile((a, b) => a-b);
   return res
     .status(200)
     .send({message: "OK"});
 });
 
-router.get('/des', logMiddleware.storeLog, function(req, res, next){
+/**
+ * Route to sort arrays descending
+ */
+router.get('/des', [authMiddleware.ensureAuthenticated, logMiddleware.storeLog], function(req, res, next){
   readOriginalFile((a, b) => b-a);
   return res
     .status(200)
     .send({message: "OK"});
 });
 
-router.get('/mix', logMiddleware.storeLog, function(req, res, next){
+/**
+ * Route to sort arrays ascending and descending in alternated way
+ */
+router.get('/mix', [authMiddleware.ensureAuthenticated, logMiddleware.storeLog], function(req, res, next){
   readOriginalFile((a, b) => a-b, true);
   return res
     .status(200)
