2fe2f8fa6ac689bf4c0e9b0de93ec08b17882938
Juank <jcgomez042@gmail.com>
2019-08-24 18:22:38 -0500

    asc and des sorting

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const authMiddleware = require('../middlewares/auth');
 const logMiddleware = require('../middlewares/sort_log');
 const User = mongoose.model('User');
+const lineReader = require('line-reader');
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -29,11 +31,60 @@ router.get('/me', authMiddleware.ensureAuthenticated, function(req, res, next) {
 	});  
 });
 
+let writeSortedFile = (arr) => {
+  let stream = fs.createWriteStream("public/assets/sorted.txt");
+  stream.once('open', function(fd) {
+    arr.forEach((item) => {
+      stream.write(`${JSON.stringify(item)};\n`);
+    });    
+    stream.end();
+  });
+} 
+
+let readOriginalFile = (nsort) => {
+  if(typeof nsort !== "function"){
+    console.log("Please, You must to give a function to sort the arrays");
+    return;
+  }
+  let narray = [];
+  lineReader.eachLine('public/assets/original.txt', function(line, last) {
+    let arr = JSON.parse(line.replace(";", ""));
+    const sortedArr = arr.sort(nsort);
+    narray.push(sortedArr);
+    if(last){
+      writeSortedFile(narray);
+    }
+  });  
+}
+
+let desc = (a, b) => {
+  if(b < a){
+    return -1;
+  }
+  if(b > a){
+    return 1;
+  }
+  return 0;
+}
+
 router.get('/asc', logMiddleware.storeLog, function(req, res, next){
+  readOriginalFile((a, b) => a-b);
   return res
     .status(200)
     .send({message: "OK"});
 });
 
+router.get('/des', logMiddleware.storeLog, function(req, res, next){
+  readOriginalFile((a, b) => b-a);
+  return res
+    .status(200)
+    .send({message: "OK"});
+});
+
+router.get('/mix', logMiddleware.storeLog, function(req, res, next){
+  return res
+    .status(200)
+    .send({message: "OK"});
+});
 
 module.exports = router;
